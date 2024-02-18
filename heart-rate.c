1b4a01160a8c5e864e2a4e94bc1fe424db18792b
Serenana007 <123705598+Serenana007@users.noreply.github.com>
2024-02-17 19:59:24 -0500

    Update heart-rate.c

--- heart-rate.c
+++ heart-rate.c
@@ -38,7 +38,7 @@ void loop() {
   heartrate.getValue(heartratePin);   // A1 foot sampled values
   rateValue = heartrate.getRate();   // Get heart rate value 
   
-  if(rateValue > 0)  {
+
     Serial.println(rateValue);
 
     // separate rateValue into hundreds, tens and ones
@@ -47,6 +47,6 @@ void loop() {
 
     // display on 7-seg
     sevseg.refreshDisplay();
-  }
+
 
 }
