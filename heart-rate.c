2603abc268ffd06cc9dd2fe35cba531355dcc860
Serenana007 <123705598+Serenana007@users.noreply.github.com>
2024-02-17 19:41:00 -0500

    Update heart-rate.c

--- heart-rate.c
+++ heart-rate.c
@@ -24,31 +24,29 @@ SevSeg sevseg;
 void setup() {
   Serial.begin(115200);
    byte numDigits = 3;
-    byte digitPins[] = {2, 3, 4, 5};
+    byte digitPins[] = {2, 3, 4};
     byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12};
     bool resistorsOnSegments = true;
 
     byte hardwareConfig = COMMON_CATHODE; 
     sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
-    sevseg.setBrightness(90);
+    sevseg.setBrightness(100);
 }
 
 void loop() {
   uint8_t rateValue;
   heartrate.getValue(heartratePin);   // A1 foot sampled values
   rateValue = heartrate.getRate();   // Get heart rate value 
+  
   if(rateValue > 0)  {
     Serial.println(rateValue);
 
-    int hundreds = rateValue/100;
-    int tens = (rateValue/10)%10;
-    int ones = rateValue % 10;
     // separate rateValue into hundreds, tens and ones
-   sevseg.setNumber (hundreds*100+tens*10+ones, 3);
 
+   sevseg.setNumber (rateValue, 2);
 
     // display on 7-seg
     sevseg.refreshDisplay();
   }
-  delay(20);
+
 }
