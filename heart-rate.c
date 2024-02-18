691f674cab108dc4598375bd479833de30365a29
BarryYu114 <139152335+BarryYu114@users.noreply.github.com>
2024-02-17 22:05:22 -0500

    Update heart-rate.c

--- heart-rate.c
+++ heart-rate.c
@@ -4,21 +4,6 @@
 
 DFRobot_Heartrate heartrate(DIGITAL_MODE);   // ANALOG_MODE or DIGITAL_MODE
 
-// define pin number and segment numbers 
-// int D1 = 2;
-// int D2 = 3;
-// int D3 = 4;
-// int D4 = 5;
-
-// int A = 6;
-// int B = 7;
-// int C = 8;
-// int D = 9;
-// int E =  10;
-// int F = 11;
-// int G = 12;
-// int DP = 13;
-
 SevSeg sevseg;
 
 void setup() {
